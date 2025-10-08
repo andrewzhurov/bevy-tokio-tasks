833500cb464a07061bbc7fa2d918e0a0db29e48c
Andrew Zhurov <zhurov.andrew@gmail.com>
2025-10-08 12:58:32 +0300

    Renames internal fields, makes runtime Res present on cb execution

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::future::Future;
-use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use bevy_app::{App, Plugin, Update};
 use bevy_ecs::schedule::{InternedScheduleLabel, ScheduleLabel};
@@ -14,17 +14,20 @@ pub use tokio;
 /// An internal struct keeping track of how many ticks have elapsed since the start of the program.
 #[derive(Resource)]
 struct UpdateTicks {
-    ticks: Arc<AtomicUsize>,
+    current_tick: Arc<AtomicUsize>,
     update_watch_tx: tokio::sync::watch::Sender<()>,
 }
 
 impl UpdateTicks {
-    fn increment_ticks(&self) -> usize {
-        let new_ticks = self.ticks.fetch_add(1, Ordering::SeqCst).wrapping_add(1);
+    fn tick(&self) -> usize {
+        let new_tick = self
+            .current_tick
+            .fetch_add(1, Ordering::SeqCst)
+            .wrapping_add(1);
         self.update_watch_tx
             .send(())
             .expect("Failed to send update_watch channel message");
-        new_ticks
+        new_tick
     }
 }
 
@@ -45,7 +48,7 @@ impl Default for TokioTasksPlugin {
     /// Configures the plugin to build a new Tokio [`Runtime`] with both IO and timer functionality
     /// enabled. On the wasm32 architecture, the [`Runtime`] will be the current-thread runtime, on all other
     /// architectures the [`Runtime`] will be the multi-thread runtime.
-    /// 
+    ///
     /// The default schedule label is [`Update`].
     fn default() -> Self {
         Self {
@@ -59,21 +62,25 @@ impl Default for TokioTasksPlugin {
                     .build()
                     .expect("Failed to create Tokio runtime for background tasks")
             }),
-            schedule_label: Update.intern()
+            schedule_label: Update.intern(),
         }
     }
 }
 
 impl Plugin for TokioTasksPlugin {
     fn build(&self, app: &mut App) {
-        let ticks = Arc::new(AtomicUsize::new(0));
+        let current_tick = Arc::new(AtomicUsize::new(0));
         let (update_watch_tx, update_watch_rx) = tokio::sync::watch::channel(());
         let runtime = (self.make_runtime)();
         app.insert_resource(UpdateTicks {
-            ticks: ticks.clone(),
+            current_tick: current_tick.clone(),
             update_watch_tx,
         });
-        app.insert_resource(TokioTasksRuntime::new(ticks, runtime, update_watch_rx));
+        app.insert_resource(TokioTasksRuntime::new(
+            current_tick,
+            runtime,
+            update_watch_rx,
+        ));
         app.add_systems(self.schedule_label, tick_runtime_update);
     }
 }
@@ -84,19 +91,14 @@ impl Plugin for TokioTasksPlugin {
 /// [`schedule_label`](TokioTasksPlugin::schedule_label) value.
 pub fn tick_runtime_update(world: &mut World) {
     let current_tick = {
-        let tick_counter = match world.get_resource::<UpdateTicks>() {
-            Some(counter) => counter,
-            None => return,
+        let Some(tick_counter) = world.get_resource::<UpdateTicks>() else {
+            return;
         };
 
         // Increment update ticks and notify watchers of update tick.
-        tick_counter.increment_ticks()
+        tick_counter.tick()
     };
-
-    if let Some(mut runtime) = world.remove_resource::<TokioTasksRuntime>() {
-        runtime.execute_main_thread_work(world, current_tick);
-        world.insert_resource(runtime);
-    }
+    execute_main_thread_work(world, current_tick);
 }
 
 type MainThreadCallback = Box<dyn FnOnce(MainThreadContext) + Send + 'static>;
@@ -110,26 +112,26 @@ pub struct TokioTasksRuntime(Box<TokioTasksRuntimeInner>);
 /// the world in [`tick_runtime_update`].
 struct TokioTasksRuntimeInner {
     runtime: Runtime,
-    ticks: Arc<AtomicUsize>,
+    current_tick: Arc<AtomicUsize>,
     update_watch_rx: tokio::sync::watch::Receiver<()>,
-    update_run_tx: tokio::sync::mpsc::UnboundedSender<MainThreadCallback>,
-    update_run_rx: tokio::sync::mpsc::UnboundedReceiver<MainThreadCallback>,
+    main_cb_tx: tokio::sync::mpsc::UnboundedSender<MainThreadCallback>,
+    main_cb_rx: tokio::sync::mpsc::UnboundedReceiver<MainThreadCallback>,
 }
 
 impl TokioTasksRuntime {
     fn new(
-        ticks: Arc<AtomicUsize>,
+        current_tick: Arc<AtomicUsize>,
         runtime: Runtime,
         update_watch_rx: tokio::sync::watch::Receiver<()>,
     ) -> Self {
-        let (update_run_tx, update_run_rx) = tokio::sync::mpsc::unbounded_channel();
+        let (main_cb_tx, main_cb_rx) = tokio::sync::mpsc::unbounded_channel();
 
         Self(Box::new(TokioTasksRuntimeInner {
             runtime,
-            ticks,
+            current_tick,
             update_watch_rx,
-            update_run_tx,
-            update_run_rx,
+            main_cb_tx,
+            main_cb_rx,
         }))
     }
 
@@ -155,28 +157,36 @@ impl TokioTasksRuntime {
         let inner = &self.0;
         let context = TaskContext {
             update_watch_rx: inner.update_watch_rx.clone(),
-            ticks: inner.ticks.clone(),
-            update_run_tx: inner.update_run_tx.clone(),
+            main_cb_tx: inner.main_cb_tx.clone(),
+            current_tick: inner.current_tick.clone(),
         };
         let future = spawnable_task(context);
         inner.runtime.spawn(future)
     }
+}
 
-    /// Execute all of the requested runnables on the main thread.
-    pub(crate) fn execute_main_thread_work(&mut self, world: &mut World, current_tick: usize) {
-        // Running this single future which yields once allows the runtime to process tasks
-        // if the runtime is a current_thread runtime. If its a multi-thread runtime then
-        // this isn't necessary but is harmless.
-        self.0.runtime.block_on(async {
+// A function, rather than a method, to not require taking `TokioTasksRuntime` out of World.
+// As World callbocks which it executes may expect it to be present.
+/// Execute all of the requested runnables on the main thread.
+pub(crate) fn execute_main_thread_work(world: &mut World, current_tick: usize) {
+    // Running this single future which yields once allows the runtime to process tasks
+    // if the runtime is a current_thread runtime. If its a multi-thread runtime then
+    // this isn't necessary but is harmless.
+    if let Some(tt_runtime) = world.get_resource::<TokioTasksRuntime>() {
+        tt_runtime.runtime().block_on(async {
             tokio::task::yield_now().await;
         });
-        while let Ok(runnable) = self.0.update_run_rx.try_recv() {
-            let context = MainThreadContext {
-                world,
-                current_tick,
-            };
-            runnable(context);
-        }
+    }
+
+    while let Some(runnable) = world
+        .get_resource_mut::<TokioTasksRuntime>()
+        .and_then(|mut tt_runtime| tt_runtime.0.main_cb_rx.try_recv().ok())
+    {
+        let context = MainThreadContext {
+            world,
+            current_tick,
+        };
+        runnable(context);
     }
 }
 
@@ -194,31 +204,23 @@ pub struct MainThreadContext<'a> {
 #[derive(Clone)]
 pub struct TaskContext {
     update_watch_rx: tokio::sync::watch::Receiver<()>,
-    update_run_tx: tokio::sync::mpsc::UnboundedSender<MainThreadCallback>,
-    ticks: Arc<AtomicUsize>,
+    main_cb_tx: tokio::sync::mpsc::UnboundedSender<MainThreadCallback>,
+    current_tick: Arc<AtomicUsize>,
 }
 
 impl TaskContext {
-    /// Returns the current value of the ticket count from the main thread - how many updates
-    /// have occurred since the start of the program. Because the tick count is updated from the
-    /// main thread, the tick count may change any time after this function call returns.
+    /// Returns the current value of the tick counter from the main thread - how many updates
+    /// have occurred since the start of the program. Because the tick counter is updated from the
+    /// main thread, it may change any time after this function call returns.
     pub fn current_tick(&self) -> usize {
-        self.ticks.load(Ordering::SeqCst)
+        self.current_tick.load(Ordering::SeqCst)
     }
 
-    /// Sleeps the background task until a given number of main thread updates have occurred. If
-    /// you instead want to sleep for a given length of wall-clock time, call the normal Tokio sleep
-    /// function.
+    /// Sleeps the background task until a given number of main thread updates have occurred.
+    /// If you instead want to sleep for a given length of wall-clock time, call the normal Tokio sleep function.
     pub async fn sleep_updates(&mut self, updates_to_sleep: usize) {
-        let target_tick = self
-            .ticks
-            .load(Ordering::SeqCst)
-            .wrapping_add(updates_to_sleep);
-        while self.ticks.load(Ordering::SeqCst) < target_tick {
-            if self.update_watch_rx.changed().await.is_err() {
-                return;
-            }
-        }
+        let target_tick = self.current_tick().wrapping_add(updates_to_sleep);
+        while self.current_tick() < target_tick && self.update_watch_rx.changed().await.is_err() {}
     }
 
     /// Invokes a synchronous callback on the main Bevy thread. The callback will have mutable access to the
@@ -231,11 +233,11 @@ impl TaskContext {
         Output: Send + 'static,
     {
         let (output_tx, output_rx) = tokio::sync::oneshot::channel();
-        if self.update_run_tx.send(Box::new(move |ctx| {
+        if self.main_cb_tx.send(Box::new(move |ctx| {
             if output_tx.send(runnable(ctx)).is_err() {
                 panic!("Failed to sent output from operation run on main thread back to waiting task");
             }
-        })).is_err() {
+         })).is_err() {
             panic!("Failed to send operation to be run on main thread");
         }
         output_rx
